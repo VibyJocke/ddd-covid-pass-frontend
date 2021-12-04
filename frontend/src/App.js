d05df65d48799748d3c8e84777388c301a62c832
Joakim Lahtinen <joakim.lahtinen@sbab.se>
2021-12-04 14:12:40 +0100

    Add vaccination button and request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
 import './App.css';
-import { Button, Modal, Form, Input } from "antd";
+import {Button, Form, Input, Modal} from "antd"
 import 'antd/dist/antd.css'
-import { useState } from "react";
+import {useState} from "react"
 import axios from "axios"
+
 function App() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const showModal = () => {
-    setIsModalVisible(true);
+  const [isRegisterModalVisible, setIsRegisterModalVisible] = useState(false)
+  const handleRegisterOk = () => {
+    axios.post("/covid-pass/patient/register", {personal_number: "12333223-3223", name: "Torkel"})
+    setIsRegisterModalVisible(false)
   };
 
-  const handleOk = () => {
-    axios.post("/covid-pass/patient/register", {personal_number:"12333223-3223"})
-    setIsModalVisible(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
+  const [isVaccinateModalVisible, setIsVaccinateModalVisible] = useState(false)
+  const handleVaccinateOk = () => {
+    axios.post("/covid-pass/patient/vaccinate", {
+      personal_number: "12333223-3223",
+      injection_date: "2021-12-04T17:45:55.9483536",
+      vaccine_type: "spikevax"
+    })
+    setIsVaccinateModalVisible(false)
   };
 
   return (
@@ -24,19 +26,38 @@ function App() {
       <header className="App-header">
         <img
           src={"https://www.lr.se/images/18.2868ebc21711e3a43527642/1586246865764/SARS-CoV-2_without_background-770px.png"}
-          className="App-logo" alt="logo" />
+          className="App-logo" alt="logo"/>
         <h1>Covid pass</h1>
         Welcome to the future of smittspridningsfixande
-        <Button onClick={showModal}>Register Patient</Button>
-        <Modal title="Register patient" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-          <Form labelAlign="right" labelCol={{ span: 12 }}>
+        <Button onClick={() => setIsRegisterModalVisible(true)}>Register Patient</Button>
+        <Modal title="Register patient"
+               visible={isRegisterModalVisible}
+               onOk={handleRegisterOk}
+               onCancel={() => setIsRegisterModalVisible(false)}>
+          <Form labelAlign="right" labelCol={{span: 12}}>
             <Form.Item name="name" label="Name">
-              <Input type="text"></Input>
+              <Input type="text"/>
             </Form.Item>
             <Form.Item name="personal_number" label="Personal number">
-              <Input type="text"></Input>
+              <Input type="text"/>
             </Form.Item>
-
+          </Form>
+        </Modal>
+        <Button onClick={() => setIsVaccinateModalVisible(true)}>Register Vaccination</Button>
+        <Modal title="Register vaccination"
+               visible={isVaccinateModalVisible}
+               onOk={handleVaccinateOk}
+               onCancel={() => setIsVaccinateModalVisible(false)}>
+          <Form labelAlign="right" labelCol={{span: 12}}>
+            <Form.Item name="personal_number" label="Personal number">
+              <Input type="text"/>
+            </Form.Item>
+            <Form.Item name="vaccine_type" label="Vaccine type">
+              <Input type="text"/>
+            </Form.Item>
+            <Form.Item name="injection_date" label="Date">
+              <Input type="date"/>
+            </Form.Item>
           </Form>
         </Modal>
       </header>
